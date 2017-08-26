689e2212d5106cd65f4d09c9442cb9497d6c86d9
dmdv <dimos-d@yandex.ru>
2017-08-26 03:14:55 +0300

    Added char coloring

--- Typescript/script.js
+++ Typescript/script.js
@@ -8,9 +8,16 @@ var ColorSample = function() {
         setInterval(function() {
             o.Color();
         }, 1e3);
+    }, o.prototype.RandomColor = function() {
+        return "rgb(" + Math.floor(255 * Math.random()) + "," + Math.floor(255 * Math.random()) + "," + Math.floor(255 * Math.random()) + ")";
     }, o.prototype.Color = function() {
-        var o = "rgb(" + Math.floor(255 * Math.random()) + "," + Math.floor(255 * Math.random()) + "," + Math.floor(255 * Math.random()) + ")";
-        document.getElementById("about").style.color = o;
+        for (var o = document.getElementById("about"), r = o.innerText; o.firstChild; ) o.removeChild(o.firstChild);
+        for (var t = this.RandomColor(), n = 0; n < r.length; n++) {
+            for (var e = void 0; e == t; ) e = this.RandomColor();
+            t = e;
+            var a = r.charAt(n), l = document.createElement("span");
+            l.innerHTML = a, l.style.color = e, o.appendChild(l);
+        }
     }, o;
 }();
 
--- Typescript/script.ts
+++ Typescript/script.ts
@@ -4,21 +4,50 @@ class ColorSample{
 
 class ColorRandomizer{
 
-    Start()
-    {
+    Start() {
+    	//this.Color();
         setInterval(() => { this.Color(); }, 1000);
-    }
+    };
 
-    Color()
-    {
-        var color =
+    RandomColor(){
 
-        "rgb("	+ Math.floor(Math.random() * 255) + ","
-        	  	+ Math.floor(Math.random() * 255) + ","
-              	+ Math.floor(Math.random() * 255) + 
-            ")";
+    	return "rgb("
+    		+ Math.floor(Math.random() * 255) + ","
+        	+ Math.floor(Math.random() * 255) + ","
+            + Math.floor(Math.random() * 255) +
+        ")";
+    };
 
-        document.getElementById("about").style.color = color;
+    Color(){
+
+        let about = document.getElementById("about");
+
+        let text = about.innerText;
+
+        while (about.firstChild) {
+        	about.removeChild(about.firstChild);
+        }
+
+        var prevColor = this.RandomColor();
+
+        for (var i = 0; i < text.length; i++){
+
+        	let color : string;
+
+        	while (color == prevColor){
+        		color = this.RandomColor();
+        	}
+
+        	prevColor = color;
+
+        	let ch = text.charAt(i);
+        	let el = document.createElement("span");
+
+        	el.innerHTML = ch;
+        	el.style.color = color;
+
+        	about.appendChild(el);
+        }
     }
 }
 
