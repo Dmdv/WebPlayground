3bb293248c55f8c8b4f3c55793a66adaf75795c2
dmdv <dimos-d@yandex.ru>
2017-08-27 16:28:38 +0300

    Removed generated folder

--- Gulp_js/src/greet.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-function sayHello(e) {
-    return "Hello from " + e + " !!!!";
-}
-
-Object.defineProperty(exports, "__esModule", {
-    value: !0
-}), exports.sayHello = sayHello;
